1490591ca0eb40f144f69983e2388872916ef622
Sovakin <fintop4ik@gmail.com>
2024-11-22 23:28:35 +0300

    push

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,5 +1,6 @@
- import Image from 'next/image';
+import Image from 'next/image';
 import styles from './page.module.css';
+import Link from 'next/link';
 
 export default function Home() {
     return (
@@ -9,31 +10,50 @@ export default function Home() {
                     <Image src="/main.png" alt="Sports" width={800} height={600} layout="responsive" />
                 </div>
                 <div className={styles.textContainer}>
-                    <h1>Добро пожаловать на наш<br/>Спортивный Календарь</h1>
+                    <h1>Добро пожаловать на наш<br />Спортивный Календарь</h1>
                     <p>
-                        Платформа для отслеживания спортивных мероприятий<br/> по всем видам спорта.
+                        Платформа для отслеживания спортивных мероприятий<br /> по всем видам спорта.
                     </p>
-                    <button className={styles.eventButton}>Список соревнований</button>
+                    <Link href="/CompetitionCalendar">
+                        <button className={styles.eventButton}>Список соревнований</button>
+                    </Link>
                 </div>
             </div>
 
-            <div className={styles.separator}></div>
+            <div className={styles.separator}>
+                <div className={styles.itemsWrap}>
+                    <div className={`${styles.items} ${styles.marquee}`}>
+                        {[...Array(10)].map((_, index) => (
+                            <div key={index} className={styles.item}>
+                                Победа Соревнования Календарь
+                            </div>
+                        ))}
+                    </div>
+                    <div aria-hidden="true" className={`${styles.items} ${styles.marquee}`}>
+                        {[...Array(10)].map((_, index) => (
+                            <div key={index} className={styles.item}>
+                                Победа Соревнования Календарь
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            </div>
 
             <section className={styles.features}>
                 <div className={styles.card}>
-                    <Image src="/filter-icon.png" alt="Filter Icon" width={80} height={80} layout="fixed" />
+                    <Image src="/filter.png" alt="Filter Icon" width={80} height={80} layout="fixed" />
                     <h2>Фильтрация мероприятий</h2>
                     <p>Используйте продвинутую фильтрацию по видам спорта, местоположению и другим параметрам.</p>
                 </div>
 
                 <div className={styles.card}>
-                    <Image src="/notification-icon.png" alt="Notification Icon" width={80} height={80} layout="fixed" />
+                    <Image src="/notification.png" alt="Notification Icon" width={80} height={80} layout="fixed" />
                     <h2>Персональные Уведомления</h2>
                     <p>Получайте уведомления о новых и измененных мероприятиях, чтобы ничего не пропустить.</p>
                 </div>
 
                 <div className={styles.card}>
-                    <Image src="/calendar-icon.png" alt="Calendar Icon" width={80} height={80} layout="fixed" />
+                    <Image src="/calendar.png" alt="Calendar Icon" width={80} height={80} layout="fixed" />
                     <h2>Удобный Календарь</h2>
                     <p>Все мероприятия в одном месте с возможностью просмотра на ближайшие недели или месяцы.</p>
                 </div>
